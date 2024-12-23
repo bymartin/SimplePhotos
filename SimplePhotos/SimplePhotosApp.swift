e17a571b08cd458009c6e9ff1eb47e48dc85a18c
Barry Martin <bymartin@users.noreply.github.com>
2024-12-23 16:52:25 -0600

    Initial Commit

---
+++ SimplePhotos/SimplePhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePhotosApp.swift
+//  SimplePhotos
+//
+//  Created by Barry Martin on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SimplePhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimplePhotosTests/SimplePhotosTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePhotosTests.swift
+//  SimplePhotosTests
+//
+//  Created by Barry Martin on 12/23/24.
+//
+
+import Testing
+@testable import SimplePhotos
+
+struct SimplePhotosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
